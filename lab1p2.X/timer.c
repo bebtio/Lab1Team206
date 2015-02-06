49f4182f2b888a5f54347737e1f99fc94d3c334d
Alberto Heras <bebtio@email.arizona.edu>
2015-02-05 17:08:17 -0700

    First Commit

---
+++ lab1p2.X/timer.c
@@ -0,0 +1,15 @@
+/*
+ * File:   timer.c
+ * Authors:
+ *
+ * Created on December 30, 2014, 8:07 PM
+ */
+
+#include "p24fj64ga002.h"
+#include "timer.h"
+
+void delayUs(unsigned int delay){
+
+    //TODO: Create a delay for "delay" micro seconds using timer 2
+    
+}
