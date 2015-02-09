e75f783d344e92dc5180936947589d0a20d9e5cc
bebtio <bebtio@email.arizona.edu>
2015-02-09 15:39:47 -0700

    Update timer.c
    Deleted Old Comments

--- lab1p1.X/timer.c
+++ lab1p1.X/timer.c
@@ -31,20 +31,7 @@ void delayUs(unsigned int delay)
     
 }
 
-/*********************************************************************************************
-// For Deboucing button press and button release
-void initTimer1()
-{
-    //TODO: Set up the 5millisecond delay with this
-    unsigned int prVal = ((FCY*waitTime)/256.0) - 1.0;
-    PR1 = prVal;
-    T1CONbits.TCKPS = 0b11; //prescalar 256
-    IEC0bits.T1IE = 1; // Enable the interrupt
-    IFS0bits.T1IF = 0; // Put the interrupt flag down
-    T1CONbits.TON = 1; // Turn the timer 1 on
-}
-
-*********************************************************************************************/
+/*********************************************************************************************/
 
 void initTimer2()
 {
